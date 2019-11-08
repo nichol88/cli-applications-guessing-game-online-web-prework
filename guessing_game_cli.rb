51ecdcbb90099e68d22f1cd68bc1261aba138cd4
Brian Nicholls <nichol88@gmail.com>
2019-11-08 21:45:58 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,9 +2,12 @@
 def run_guessing_game
   num = rand(5) + 1
 
-  #puts "Guess a number!"
+  puts "Guess a number!"
 
   usr_num = gets.chomp
+
+  puts "Computer's guess: #{num}"
+  puts "User guessed #{usr_num}"
 
   if usr_num == num
     puts "You guessed the correct number!"
